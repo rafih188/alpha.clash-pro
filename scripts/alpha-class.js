eb3af0e855a8a83bd00bf78ad468253a9837dca6
rafih188 <rafeh1888@gmail.com>
2024-02-16 15:54:44 +0600

    added script language

--- scripts/alpha-class.js
+++ scripts/alpha-class.js
@@ -3,7 +3,18 @@
 // const addClassList = homeSection.classList.add('hidden');
 // const removeClassList= document.getElementById('playground-screen').classList.remove('hidden');
 // }
+function continueGame(){
+const Alphabet = getARandomAlphabet();
+
+const currentAlphabet = document.getElementById('current-alphabet');
+const showAlphabet = currentAlphabet.innerText = Alphabet;
+setBackgroundColorById(Alphabet)
+
+console.log( "your random alphabet :",Alphabet);
+}
+
 function play( elementId){
     hideElementById('home-screen')
     showElementById('playground-screen')
+    continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,6 +1,30 @@
-function hideElementById(elementId){
-   const element =  document.getElementById(elementId).classList.add('hidden');
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId).classList.add('hidden');
 }
-function showElementById(elementId){
+function showElementById(elementId) {
     const hideEle = document.getElementById(elementId).classList.remove('hidden');
+}
+
+function getARandomAlphabet() {
+    // get or create alphabet 
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // get random number 
+    const randomNumber = Math.random() * 25;
+    const number = Math.round(randomNumber);
+
+    const finalAlphabet = alphabets[number];
+    // console.log( number,finalAlphabet);
+    return finalAlphabet;
+}
+
+function setBackgroundColorById(elementId) {
+    const elementt = document.getElementById(elementId);
+    const alphabetStyle = elementt.classList.add('bg-orange-400');
+    elementt.classList.add('text-black')
+
+}
+
+function removeBackgroundColorById(elementId) {
+    const removedBackground = document.getElementById(elementId).classList.remove('bg-orange-400')
 }
